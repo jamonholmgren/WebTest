3b66e46e8bda1b73400d2eddfd5c78e4df973193
Jamon Holmgren <jamon@infinite.red>
2019-09-03 15:59:36 +0200

    iOS and Android detox tests passing

--- App.js
+++ App.js
@@ -1,41 +1,70 @@
-import React, {Fragment} from 'react';
-import {SafeAreaView, StatusBar, Alert} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {View, Text, StatusBar, Button} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 const App = () => {
+  const ref = useRef(undefined);
+  const [hello, setHello] = useState(false);
+
   const html = `
     <!DOCTYPE html>
     <html lang="en">
       <head>
         <title>WebTest web view</title>
         <script>
-          window.ReactNativeWebView.postMessage("Hey!")
+          function sendData() {
+            window.ReactNativeWebView.postMessage("Hey!")
+          }
         </script>
+        <style>
+          main {
+            padding: 150px;
+            font-size: 50px;
+          }
+          main button {
+            font-size: 40px;
+          }
+        </style>
       </head>
       <body>
-        <h1>Hello, World!</h1>
-        <button onClick="sendData">Send Data</button>
-      </body>
+        <main>
+          <h1>Hello, World!</h1>
+          <button onClick="sendData()">Send Data</button>
+        </div>
+      </main>
     </html>
   `;
+
+  const triggerHello = () => {
+    ref.current.injectJavaScript('sendData(); true');
+  };
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView testID="welcome">
-        <WebView
-          testID="test_webview"
-          source={{html}}
-          style={{
-            flex: 1,
-            height: 300,
-          }}
-          onMessage={data => {
-            Alert.alert('Message received');
-            console.log(data);
-          }}
+      <WebView
+        testID="test_webview"
+        source={{html}}
+        ref={ref}
+        style={{
+          flex: 3,
+        }}
+        onMessage={event => {
+          setHello(true);
+        }}
+      />
+      <View style={{flex: 1}}>
+        {hello ? (
+          <Text testID="hello">Hello from the webview!</Text>
+        ) : (
+          <Text testID="hello">No hello yet</Text>
+        )}
+        <Button
+          testID="test_trigger_button"
+          onPress={triggerHello}
+          title="Trigger Hello"
         />
-      </SafeAreaView>
+      </View>
     </>
   );
 };
--- e2e/testWebView.spec.js
+++ e2e/testWebView.spec.js
@@ -1,10 +1,19 @@
-describe('Example', () => {
+describe('WebView basic test', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
   it('should have a webview', async () => {
     await expect(element(by.id('test_webview'))).toBeVisible();
+  });
+
+  it('should respond to injectJavaScript', async () => {
+    const hello = await element(by.id('hello'));
+    await expect(hello).toHaveText('No hello yet');
+    await element(by.id('test_trigger_button')).tap();
+    await waitFor(hello)
+      .toHaveText('Hello from the webview!')
+      .withTimeout(2000);
   });
 
   // it('should show hello screen after tap', async () => {
