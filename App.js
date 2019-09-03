35ef2c36ed58ecb93bf0ea6be3a35f754a5daf30
Jamon Holmgren <jamon@infinite.red>
2019-09-03 14:09:59 +0200

    Added detox

--- App.js
+++ App.js
@@ -6,67 +6,40 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
+  const [welcome, setWelcome] = useState(false);
+  const [world, setWorld] = useState(false);
+
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <SafeAreaView testID="welcome">
+        <View>
+          <TouchableOpacity
+            onPress={() => setWelcome(true)}
+            testID="hello_button">
+            <Text>Show hello</Text>
+          </TouchableOpacity>
+          {welcome && <Text>Hello!!!</Text>}
+          <TouchableOpacity
+            onPress={() => setWorld(true)}
+            testID="world_button">
+            <Text>Show world</Text>
+          </TouchableOpacity>
+          {world && <Text>World!!!</Text>}
+        </View>
       </SafeAreaView>
     </Fragment>
   );
