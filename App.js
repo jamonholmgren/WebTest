d48f2c935c9048ff44aaa32655b9bffa85efa7ac
Jamon Holmgren <jamon@infinite.red>
2019-09-03 15:16:18 +0200

    Added Android detox

--- App.js
+++ App.js
@@ -1,88 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment, useState} from 'react';
-
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  TouchableOpacity,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {Fragment} from 'react';
+import {SafeAreaView, StatusBar, Alert} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const App = () => {
-  const [welcome, setWelcome] = useState(false);
-  const [world, setWorld] = useState(false);
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>WebTest web view</title>
+        <script>
+          window.ReactNativeWebView.postMessage("Hey!")
+        </script>
+      </head>
+      <body>
+        <h1>Hello, World!</h1>
+        <button onClick="sendData">Send Data</button>
+      </body>
+    </html>
+  `;
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView testID="welcome">
-        <View>
-          <TouchableOpacity
-            onPress={() => setWelcome(true)}
-            testID="hello_button">
-            <Text>Show hello</Text>
-          </TouchableOpacity>
-          {welcome && <Text>Hello!!!</Text>}
-          <TouchableOpacity
-            onPress={() => setWorld(true)}
-            testID="world_button">
-            <Text>Show world</Text>
-          </TouchableOpacity>
-          {world && <Text>World!!!</Text>}
-        </View>
+        <WebView
+          testID="test_webview"
+          source={{html}}
+          style={{
+            flex: 1,
+            height: 300,
+          }}
+          onMessage={data => {
+            Alert.alert('Message received');
+            console.log(data);
+          }}
+        />
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
